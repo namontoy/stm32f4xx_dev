a0f002b2c6ea2f2c818ac4de1f8580014eb3a105
Nerio Andrés Montoya G <namontoy@unal.edu.co>
2024-08-26 20:04:15 -0500

    Update i2c_driver_hal.h
    Cambiando completamente el driver del I2c

--- PeripheralDrivers/Inc/i2c_driver_hal.h
+++ PeripheralDrivers/Inc/i2c_driver_hal.h
@@ -11,52 +11,45 @@
 #include <stm32f4xx.h>
 #include "gpio_driver_hal.h"
 
-#define I2C_WRITE_DATA		0
-#define I2C_READ_DATA		1
+enum{
+	eI2C_WRITE_DATA = 0,
+	eI2C_READ_DATA
+};
 
-#define MAIN_CLOCK_4_MHz_FOR_I2C	4
-#define MAIN_CLOCK_16_MHz_FOR_I2C	16
-#define MAIN_CLOCK_20_MHz_FOR_I2C	20
+enum{
+	eI2C_MODE_SM = 0,
+	eI2C_MODE_FM
+};
 
-#define I2C_MODE_SM			0
-#define I2C_MODE_FM			1
+#define I2C_MAIN_CLOCK_4_MHz	4
+#define I2C_MAIN_CLOCK_16_MHz	16
+#define I2C_MAIN_CLOCK_20_MHz	20
 
-#define I2C_MODE_SM_SPEED_100KHz	80
-#define I2C_MODE_FM_SPEED_400KHz	13
+#define I2C_MODE_SM_SPEED	80  // 100 KHz clock signal
+#define I2C_MODE_FM_SPEED	13	// 400 KHz clock signal
 
-#define I2C_MAX_RISE_TIME_SM		17
-#define I2C_MAX_RISE_TIME_FM		5
-
+#define I2C_MAX_RISE_TIME_SM	17
+#define I2C_MAX_RISE_TIME_FM	5
 
 
 typedef struct
 {
-	I2C_TypeDef		*ptrI2Cx;
-	uint8_t			slaveAddress;
-	uint8_t			modeI2C;
-	uint8_t			dataI2C;
+	I2C_TypeDef	*pI2Cx;
+	uint8_t		slaveAddress;
+	uint8_t		i2c_mode;
+	uint8_t		i2c_mainClock;
+	uint8_t		i2c_data;
 }I2C_Handler_t;
 
-/*Prototipos de las funciones públicas */
-void i2c_config(I2C_Handler_t *ptrHandlerI2C);
-void i2c_startTransaction(I2C_Handler_t *ptrHandlerI2C);
-void i2c_reStartTransaction(I2C_Handler_t *ptrHandlerI2C);
-void i2c_sendSlaveAddressRW(I2C_Handler_t *ptrHandlerI2C, uint8_t slaveAddress, uint8_t readOrWrite);
-void i2c_sendMemoryAddress(I2C_Handler_t *ptrHandlerI2C, uint8_t memAddr);
-void i2c_sendDataByte(I2C_Handler_t *ptrHandlerI2C, uint8_t dataToWrite);
-uint8_t i2c_readDataByte(I2C_Handler_t *ptrHandlerI2C);
-void i2c_stopTransaction(I2C_Handler_t *ptrHandlerI2C);
-void i2c_sendAck(I2C_Handler_t *ptrHandlerI2C);
-void i2c_sendNoAck(I2C_Handler_t *ptrHandlerI2C);
-void i2c_softReset(I2C_Handler_t *ptrHandlerI2C);
 
-void i2c_setPins(GPIO_Handler_t	*setSdaPin, GPIO_Handler_t	*setSclPin);
-void i2c_clearBusyFlagState(I2C_Handler_t *ptrHandlerI2C);
+/* Prototipos de las funciones publicas */
+void i2c_Config(I2C_Handler_t *pHandlerI2C);
+uint8_t i2c_ReadSingleRegister(I2C_Handler_t *pHandlerI2C, uint8_t regToRead);
+uint8_t i2c_ReadManyRegisters(I2C_Handler_t *pHandlerI2C, uint8_t regToRead, uint8_t *bufferData, uint8_t numberOfBytes);
+void i2c_WriteSingleRegister(I2C_Handler_t *pHandlerI2C, uint8_t regToWrite, uint8_t newValue);
+void i2c_WriteManyRegisters(I2C_Handler_t *pHandlerI2C, uint8_t regToWrite, uint8_t *bufferRxData, uint8_t numberOfBytes);
 
-uint8_t i2c_readSingleRegister(I2C_Handler_t *ptrHandlerI2C, uint8_t regToRead);
-uint8_t i2c_readManyRegisters(I2C_Handler_t *ptrHandlerI2C, uint8_t regToRead, uint8_t *bufferData, uint8_t numberOfBytes);
-void i2c_writeSingleRegister(I2C_Handler_t *ptrHandlerI2C, uint8_t regToWrite, uint8_t newValue);
-void i2c_writeManyRegisters(I2C_Handler_t *ptrHandlerI2C, uint8_t regToWrite, uint8_t *bufferRxData, uint8_t numberOfBytes);
-
+//void i2c_setPins(GPIO_Handler_t	*setSdaPin, GPIO_Handler_t	*setSclPin);
+//void i2c_clearBusyFlagState(I2C_Handler_t *ptrHandlerI2C);
 
 #endif /* I2C_DRIVER_HAL_H_ */
